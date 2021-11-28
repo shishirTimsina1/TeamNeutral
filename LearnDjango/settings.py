e69fd7dbc544a16b3e5f264d6ecc902e50a7493d
sht99 <44593416+sht99@users.noreply.github.com>
2021-11-28 14:04:31 -0500

    everything but communities is done

--- LearnDjango/settings.py
+++ LearnDjango/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'events.apps.EventsConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
     'blog.apps.BlogConfig',
--- users/models.py
+++ users/models.py
@@ -6,6 +6,8 @@ from PIL import Image
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	image = models.ImageField(default='default.jpg', upload_to = 'profile_pics')
+	#following = models.ManyToManyField(User, related_name = 'following', blank=True)
+
 
 	def __str__(self):
 		return f'{self.user.username} Profile'
