5a1c94901f36ec213c2d31abaee20f1df6829c19
Chris Votilla <cmv43@pitt.edu>
2021-11-29 17:08:45 -0500

    Implemented Communities

--- communities/urls.py
+++ communities/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 from . import views
+from .views import mortalKombatPosts, mortalKombatEvents, SmashEvents, SmashPosts, AllEvents, AllPosts, GGEvents, GGPosts, SFEvents, SFPosts
 from blog.models import Post
 
 urlpatterns = [
 	path('', views.home, name = 'communities'),
+	path('AllPosts/', AllPosts, name = 'All-posts'),
+	path('AllEvents/', AllEvents, name = 'All-events'),
+	path('GuiltyGearPosts/', GGPosts, name = 'GG-posts'),
+	path('GuiltyGearEvents/', GGEvents, name = 'GG-events'),
+	path('StreetFighterPosts/', SFPosts, name = 'SF-posts'),
+	path('StreetFighterEvents/', SFEvents, name = 'SF-events'),
+	path('SmashPosts/', SmashPosts, name = 'Smash-posts'),
+	path('SmashEvents/', SmashEvents, name = 'Smash-events'),
+	path('MortalKombatPosts/', mortalKombatPosts, name = 'MK-posts'),
+	path('MortalKombatEvents/', mortalKombatEvents, name = 'MK-events'),
 ]
--- communities/views.py
+++ communities/views.py
@@ -4,6 +4,8 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse
 from blog.models import Post
+from django.db.models import Q
+from events.models import Events
 
 # Create your views here.
 
@@ -12,3 +14,63 @@ def home(request):
 		'posts': Post.objects.all()
 	}
 	return render(request, 'communities/community-home.html', context)
+
+def AllPosts(request):
+	context = {
+		'posts': Post.objects.order_by('-date_posted').all()
+	}
+	return render(request, 'communities/All-posts.html', context)
+
+def AllEvents(request):
+	context = {
+		'events': Events.objects.order_by('-date_posted').all()
+	}
+	return render(request, 'communities/All-events.html', context)
+
+def mortalKombatPosts(request):
+	context = {
+		'posts': Post.objects.filter(Q(community = 'Mortal Kombat') | Q(community='All')).order_by('-date_posted').all()
+	}
+	return render(request, 'communities/mortalKombat-posts.html', context)
+
+def mortalKombatEvents(request):
+	context = {
+		'events': Events.objects.filter(Q(community = 'Mortal Kombat') | Q(community='All')).order_by('-date_posted').all()
+	}
+	return render(request, 'communities/mortalKombat-events.html', context)
+
+def SmashPosts(request):
+	context = {
+		'posts': Post.objects.filter(Q(community = 'Smash') | Q(community='All')).order_by('-date_posted').all()
+	}
+	return render(request, 'communities/Smash-posts.html', context)
+
+def SmashEvents(request):
+	context = {
+		'events': Events.objects.filter(Q(community = 'Smash') | Q(community='All')).order_by('-date_posted').all()
+	}
+	return render(request, 'communities/Smash-events.html', context)
+
+def GGPosts(request):
+	context = {
+		'posts': Post.objects.filter(Q(community = 'Guilty Gear') | Q(community='All')).order_by('-date_posted').all()
+	}
+	return render(request, 'communities/GG-posts.html', context)
+
+def GGEvents(request):
+	context = {
+		'events': Events.objects.filter(Q(community = 'Guilty Gear') | Q(community='All')).order_by('-date_posted').all()
+	}
+	return render(request, 'communities/GG-events.html', context)
+
+def SFPosts(request):
+	context = {
+		'posts': Post.objects.filter(Q(community = 'Street Fighter') | Q(community='All')).order_by('-date_posted').all()
+	}
+	return render(request, 'communities/SF-posts.html', context)
+
+def SFEvents(request):
+	context = {
+		'events': Events.objects.filter(Q(community = 'Street Fighter') | Q(community='All')).order_by('-date_posted').all()
+	}
+	return render(request, 'communities/SF-events.html', context)
