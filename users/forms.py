aa055fe332ba7a493687f61f8d4fced23ca17bb9
Chris Votilla <cmv43@pitt.edu>
2021-12-01 09:26:23 -0500

    fixed some of the test issues

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 from .models import Profile
 
 
@@ -10,6 +11,13 @@ class UserRegisterForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
+	
+	def clean(self):
+		email = self.cleaned_data.get('email')
+		if User.objects.filter(email=email).exists():
+			raise ValidationError("Another user is already using that email")
+		return self.cleaned_data
+
 
 class UserUpdateForm(forms.ModelForm):
 	email = forms.EmailField(required=True)
