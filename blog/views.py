8c087e1b91112f16cc1dc0ef2682fedd6c31c0aa
sht99 <44593416+sht99@users.noreply.github.com>
2021-12-07 18:53:29 -0500

    small changes

--- blog/views.py
+++ blog/views.py
@@ -45,7 +45,6 @@ class UserPostListView(ListView):
 	model = Post
 	template_name = 'blog/user_posts.html' #<app>/<model>_<viewtype>.html
 	context_object_name = 'posts'
-	
 	paginate_by = 5
 
 	def get_queryset(self):
