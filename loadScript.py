e26705c98e3ffcb560f50299d3c8711aaaaf5085
sht99 <44593416+sht99@users.noreply.github.com>
2021-12-07 21:03:51 -0500

    Pushing database setup for demo

--- loadScript.py
+++ loadScript.py
@@ -1,9 +1,14 @@
+#exec(open('loadScript.py').read())
+
 import json
 from blog.models import Post
 
+#use posts.json or posts2.json
 with open('posts.json') as f:
 	posts_json = json.load(f)
 
 for post in posts_json:
 	post = Post(title=post['title'], content=post['content'], community=post['community'], author_id=post['user_id'])
 	post.save()
+
+
